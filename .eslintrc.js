d888ddede2fe490bbec01bbd5b567e61db5d5f0f
woodghost <tingyue@vite.org>
2019-04-15 22:14:51 +0800

    add eslint & ignore

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  plugins: [
+    'html'
+  ],
+  env: {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    'ecmaFeatures': {
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  rules: {
+    "no-console": [
+      "off"
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  },
+  "globals": {
+    "window": true
+  },
+};
---
+++ config.js
@@ -0,0 +1,125 @@
+module.exports = {
+  urls: {
+    weibo: 'https://weibo.com/u/6572727462',
+    viteNet: {
+      zh: 'https://vite.net/zh',
+      en: 'https://vite.net',
+      ru: 'https://vite.net',
+      vi: 'https://vite.net'
+    },
+    explorer: {
+      zh: 'https://explorer.vite.net/zh',
+      en: 'https://explorer.vite.net',
+      ru: 'https://explorer.vite.net',
+      vi: 'https://explorer.vite.net'
+    },
+    webWallet: {
+      zh: 'https://wallet.vite.net',
+      en: 'https://wallet.vite.net',
+      ru: 'https://wallet.vite.net',
+      vi: 'https://wallet.vite.net'
+    },
+    news: {
+      rss: {
+        zh: 'https://hidden-peak-43038.herokuapp.com/https://forum.vite.net/category/18.rss',
+        en: 'https://hidden-peak-43038.herokuapp.com/https://medium.com/feed/vitelabs', // crossorigin via
+        ru: 'https://hidden-peak-43038.herokuapp.com/https://medium.com/feed/vitelabs',
+        vi: 'https://hidden-peak-43038.herokuapp.com/https://medium.com/feed/vitelabs'
+      },
+      home: {
+        zh: 'https://forum.vite.net/category/18/%E5%85%AC%E5%91%8A',
+        en: 'https://medium.com/vitelabs',
+        ru: 'https://medium.com/vitelabs',
+        vi: 'https://medium.com/vitelabs'
+      }
+    },
+    forum: 'https://forum.vite.net',
+    video: {
+      home: {
+        zh: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/72d670585285890783561692168/WGwQoGPaGHsA.mp4',
+        en: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/6e1ba7d25285890783561485090/pnzXKMBXtB4A.mp4',
+        ru: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/6e1ba7d25285890783561485090/pnzXKMBXtB4A.mp4',
+        vi: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/6e1ba7d25285890783561485090/pnzXKMBXtB4A.mp4'
+      },
+      loves: {
+        zh: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/6dbf98485285890783561432421/ZKiKb7ZBzq4A.mp4',
+        en: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/696fc14d5285890783561282405/iunXawfrnzoA.mp4',
+        ru: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/696fc14d5285890783561282405/iunXawfrnzoA.mp4',
+        vi: 'http://1257137467.vod2.myqcloud.com/c9d24a0dvodcq1257137467/696fc14d5285890783561282405/iunXawfrnzoA.mp4'
+      }
+    },
+    twitter: 'https://twitter.com/vitelabs',
+    discord: 'https://discordapp.com/invite/CsVY76q',
+    reddit: 'https://www.reddit.com/r/vitelabs',
+    github: 'https://github.com/vitelabs',
+    document: 'https://vite.wiki',
+    loopring: 'https://loopring.org',
+    faq: '/faq',
+    medium: 'https://medium.com/vitelabs',
+    tokenUrl: 'https://etherscan.io/token/0x1b793E49237758dBD8b752AFC9Eb4b329d5Da016',
+    btt: 'https://bitcointalk.org/index.php?topic=4438416',
+    youtube: 'https://www.youtube.com/channel/UC8qft2rEzBnP9yJOGdsJBVg',
+    testnet: 'https://vite.net',
+    introduction: {
+      en: 'https://vite.wiki/introduction/',
+      zh: 'https://vite.wiki/zh/introduction/',
+      ru: 'https://vite.wiki/introduction/',
+      vi: 'https://vite.wiki/introduction/'
+    },
+    wiki: {
+      en: 'https://vite.wiki/introduction/',
+      zh: 'https://vite.wiki/zh/introduction/',
+      ru: 'https://vite.wiki/introduction/',
+      vi: 'https://vite.wiki/introduction/'
+    },
+    blog: {
+      en: 'https://vite.blog/',
+      zh: 'https://vite.blog/zh-cn/',
+      ru: 'https://vite.blog/',
+      vi: 'https://vite.blog/'
+    },
+    telegram: {
+      ann: 'https://t.me/vite_ann',
+      english: 'https://t.me/vite_en',
+      chinese: 'https://t.me/vite_zh',
+      vietnamese: 'https://t.me/vite_vietnamese',
+      korean: 'https://t.me/vite_korean',
+      russia: 'https://t.me/vite_russia',
+      thailand: 'https://t.me/vite_thailand'
+    },
+    whitePaper: {
+      english: 'https://github.com/vitelabs/whitepaper/blob/master/vite_en.pdf',
+      chinese: 'https://github.com/vitelabs/whitepaper/blob/master/vite_cn.pdf',
+      french: 'https://github.com/vitelabs/whitepaper/blob/master/vite_fr.pdf',
+      korean: 'https://github.com/vitelabs/whitepaper/blob/master/vite_korean.pdf',
+      vietnamese: 'https://github.com/vitelabs/whitepaper/blob/master/vite_vietnamese.pdf',
+      russian: 'https://github.com/vitelabs/whitepaper/blob/master/vite_russian.pdf',
+      japan: 'https://github.com/vitelabs/whitepaper/blob/master/vite_japan.pdf'
+    },
+    exchange: {
+      bittrex: 'https://international.bittrex.com/Market/Index?MarketName=BTC-VITE',
+      okex: 'https://www.okex.com/spot/trade#product=vite_eth',
+      upbit: 'https://sg.upbit.com/exchange?code=CRIX.UPBIT.BTC-VITE'
+    },
+    media: {
+      medium: 'https://medium.com/vitelabs',
+      bitcointalk: 'https://bitcointalk.org/index.php?topic=5056409',
+      forum: 'https://forum.vite.net'
+    },
+    vote: {
+      specific: {
+        en: '/regSpecificEn',
+        zh: '/regSpecificCh',
+        ru: '/regSpecificEn',
+        vi: '/regSpecificEn'
+      },
+      apply: {
+        en: 'https://docs.google.com/forms/d/e/1FAIpQLScixWTDuId0MCJQGi0jVI3LCivQ-r5fNUMFCnLf-uodIc_EsA/viewform',
+        zh: 'https://www.jiandaoyun.com/f/5bdbef8be1613237783c264b',
+        ru: 'https://docs.google.com/forms/d/e/1FAIpQLScixWTDuId0MCJQGi0jVI3LCivQ-r5fNUMFCnLf-uodIc_EsA/viewform',
+        vi: 'https://docs.google.com/forms/d/e/1FAIpQLScixWTDuId0MCJQGi0jVI3LCivQ-r5fNUMFCnLf-uodIc_EsA/viewform'
+      }
+
+    }
+  }
+};
