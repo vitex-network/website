355dd92fdb1cb7a4f04aadd44faeb332668db312
zhoudan <zhoudan@vite.org>
2019-09-09 20:16:32 +0800

    change header

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,7 +31,7 @@
         </div>
 
         <div
-            class="navbar-menu column is-7-desktop is-offset-1"
+            class="navbar-menu column is-8-desktop is-offset-2"
             :class="{ 'is-active': navbarActive, collapsing: collapsing }"
         >
           <div
