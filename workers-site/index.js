ee88665dac89f32d37bc0ea5c9af97e87bb14f69
soliury <soliury@pm.me>
2020-09-13 15:22:49 +0800

    add browser cache

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,4 +1,4 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler';
 
 /**
  * The DEBUG flag will do two things that help during development:
@@ -7,26 +7,37 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
-const DEBUG = false
+const DEBUG = false;
+const browserCacheList = [ '.js', '.css', '.otf', '.ttf', '.ttc' ];
 
 addEventListener('fetch', event => {
   try {
-    event.respondWith(handleEvent(event))
+    event.respondWith(handleEvent(event));
   } catch (e) {
     if (DEBUG) {
       return event.respondWith(
         new Response(e.message || e.toString(), {
           status: 500,
         }),
-      )
+      );
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
+    event.respondWith(new Response('Internal Error', { status: 500 }));
   }
-})
+});
+
+
+function mapRequestCacheControl(request) {
+  const url = new URL(request.url);
+  if (browserCacheList.some(item => url.pathname.endsWith(item))) {
+    return { browserTTL: 60 * 60 * 24 * 365, edgeTTL: 2 * 60 * 60 * 24 };
+  }
+  return { edgeTTL: 2 * 60 * 60 * 24 };
+}
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
+  let options = {
+    cacheControl: mapRequestCacheControl
+  };
 
   /**
    * You can add custom logic to how we fetch your assets
@@ -39,22 +50,22 @@ async function handleEvent(event) {
       // customize caching
       options.cacheControl = {
         bypassCache: true,
-      }
+      };
     }
-    return await getAssetFromKV(event, options)
+    return await getAssetFromKV(event, options);
   } catch (e) {
     // if an error is thrown try to serve the asset at 404.html
     if (!DEBUG) {
       try {
         let notFoundResponse = await getAssetFromKV(event, {
           mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
+        });
 
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
       } catch (e) {}
     }
 
-    return new Response(e.message || e.toString(), { status: 500 })
+    return new Response(e.message || e.toString(), { status: 500 });
   }
 }
 
@@ -68,13 +79,13 @@ async function handleEvent(event) {
 function handlePrefix(prefix) {
   return request => {
     // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
+    let defaultAssetKey = mapRequestToAsset(request);
+    let url = new URL(defaultAssetKey.url);
 
     // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
+    url.pathname = url.pathname.replace(prefix, '/');
 
     // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
-  }
+    return new Request(url.toString(), defaultAssetKey);
+  };
 }
